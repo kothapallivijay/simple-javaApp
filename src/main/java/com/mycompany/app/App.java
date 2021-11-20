bc811a60ca834dd6a6f204f6cb9455887464459a
kothapallivijay <89311193+kothapallivijay@users.noreply.github.com>
2021-11-20 14:17:47 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello kiran kumar" );
+        System.out.println( "Hello kiran kumar ji" );
     }
 }
